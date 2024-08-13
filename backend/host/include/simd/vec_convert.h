9aafe4cb897034c18b55d1441bf38c65e1fe4eb5
BL-GS <13413419347@163.com>
2024-08-14 00:11:25 +0800

    refactor: replace raw simd operator with simde

--- backend/host/include/simd/vec_convert.h
+++ backend/host/include/simd/vec_convert.h
@@ -56,7 +56,7 @@ namespace spy::cpu {
 		static void transform(const FromBlock * __restrict from_ptr, ToBlock * __restrict to_ptr, size_t num_from) {
 			 int64_t i = 0;
 			 for (; i + 7 < num_from; i += 8) {
-			 	simde__m256 x_vec  = _mm256_loadu_ps(from_ptr + i);
+			 	simde__m256 x_vec  = simde_mm256_loadu_ps(from_ptr + i);
 				simde__m128i y_vec = simde_mm256_cvtps_ph(x_vec, SIMDE_MM_FROUND_TO_NEAREST_INT);
 			 	simde_mm_storeu_si128(to_ptr + i, y_vec);
 			 }
@@ -201,61 +201,61 @@ namespace spy::cpu {
 
 			for (size_t i = 0; i < num_from_block; ++i) {
 				// Load elements into 4 AVX vectors
-				__m256 v0 = _mm256_loadu_ps( from_ptr );
-				__m256 v1 = _mm256_loadu_ps( from_ptr + 8 );
-				__m256 v2 = _mm256_loadu_ps( from_ptr + 16 );
-				__m256 v3 = _mm256_loadu_ps( from_ptr + 24 );
+				simde__m256 v0 = simde_mm256_loadu_ps( from_ptr );
+				simde__m256 v1 = simde_mm256_loadu_ps( from_ptr + 8 );
+				simde__m256 v2 = simde_mm256_loadu_ps( from_ptr + 16 );
+				simde__m256 v3 = simde_mm256_loadu_ps( from_ptr + 24 );
 				from_ptr += 32;
 
 				// Compute max(abs(e)) for the block
-				const __m256 signBit = _mm256_set1_ps( -0.0F );
-				__m256 maxAbs = _mm256_andnot_ps( signBit, v0 );
-				maxAbs = _mm256_max_ps( maxAbs, _mm256_andnot_ps( signBit, v1 ) );
-				maxAbs = _mm256_max_ps( maxAbs, _mm256_andnot_ps( signBit, v2 ) );
-				maxAbs = _mm256_max_ps( maxAbs, _mm256_andnot_ps( signBit, v3 ) );
+				const simde__m256 signBit = simde_mm256_set1_ps( -0.0F );
+				simde__m256 maxAbs = simde_mm256_andnot_ps( signBit, v0 );
+				maxAbs = simde_mm256_max_ps( maxAbs, simde_mm256_andnot_ps( signBit, v1 ) );
+				maxAbs = simde_mm256_max_ps( maxAbs, simde_mm256_andnot_ps( signBit, v2 ) );
+				maxAbs = simde_mm256_max_ps( maxAbs, simde_mm256_andnot_ps( signBit, v3 ) );
 
-				__m128 max4 = _mm_max_ps( _mm256_extractf128_ps( maxAbs, 1 ), _mm256_castps256_ps128( maxAbs ) );
-				max4 = _mm_max_ps( max4, _mm_movehl_ps( max4, max4 ) );
-				max4 = _mm_max_ss( max4, _mm_movehdup_ps( max4 ) );
+				simde__m128 max4 = simde_mm_max_ps( simde_mm256_extractf128_ps( maxAbs, 1 ), simde_mm256_castps256_ps128( maxAbs ) );
+				max4 = simde_mm_max_ps( max4, simde_mm_movehl_ps( max4, max4 ) );
+				max4 = simde_mm_max_ss( max4, simde_mm_movehdup_ps( max4 ) );
 				const float maxScalar = _mm_cvtss_f32( max4 );
 
 				// Quantize these floats
 				const float d = maxScalar / 127.F;
 				to_ptr[i].delta = spy_fp32_to_fp16(d);
 				const float id = ( maxScalar != 0.0F ) ? 127.0F / maxScalar : 0.0F;
-				const __m256 mul = _mm256_set1_ps( id );
+				const simde__m256 mul = simde_mm256_set1_ps( id );
 
 				// Apply the multiplier
-				v0 = _mm256_mul_ps( v0, mul );
-				v1 = _mm256_mul_ps( v1, mul );
-				v2 = _mm256_mul_ps( v2, mul );
-				v3 = _mm256_mul_ps( v3, mul );
+				v0 = simde_mm256_mul_ps( v0, mul );
+				v1 = simde_mm256_mul_ps( v1, mul );
+				v2 = simde_mm256_mul_ps( v2, mul );
+				v3 = simde_mm256_mul_ps( v3, mul );
 
 				// Round to nearest integer
-				v0 = _mm256_round_ps( v0, _MM_ROUND_NEAREST );
-				v1 = _mm256_round_ps( v1, _MM_ROUND_NEAREST );
-				v2 = _mm256_round_ps( v2, _MM_ROUND_NEAREST );
-				v3 = _mm256_round_ps( v3, _MM_ROUND_NEAREST );
+				v0 = simde_mm256_round_ps( v0, SIMDE_MM_ROUND_NEAREST );
+				v1 = simde_mm256_round_ps( v1, SIMDE_MM_ROUND_NEAREST );
+				v2 = simde_mm256_round_ps( v2, SIMDE_MM_ROUND_NEAREST );
+				v3 = simde_mm256_round_ps( v3, SIMDE_MM_ROUND_NEAREST );
 
 				// Convert floats to integers
-				__m256i i0 = _mm256_cvtps_epi32( v0 );
-				__m256i i1 = _mm256_cvtps_epi32( v1 );
-				__m256i i2 = _mm256_cvtps_epi32( v2 );
-				__m256i i3 = _mm256_cvtps_epi32( v3 );
+				simde__m256i i0 = simde_mm256_cvtps_epi32( v0 );
+				simde__m256i i1 = simde_mm256_cvtps_epi32( v1 );
+				simde__m256i i2 = simde_mm256_cvtps_epi32( v2 );
+				simde__m256i i3 = simde_mm256_cvtps_epi32( v3 );
 
 				// Convert int32 to int16
-				i0 = _mm256_packs_epi32( i0, i1 );	// 0, 1, 2, 3,  8, 9, 10, 11,  4, 5, 6, 7, 12, 13, 14, 15
-				i2 = _mm256_packs_epi32( i2, i3 );	// 16, 17, 18, 19,  24, 25, 26, 27,  20, 21, 22, 23, 28, 29, 30, 31
+				i0 = simde_mm256_packs_epi32( i0, i1 );	// 0, 1, 2, 3,  8, 9, 10, 11,  4, 5, 6, 7, 12, 13, 14, 15
+				i2 = simde_mm256_packs_epi32( i2, i3 );	// 16, 17, 18, 19,  24, 25, 26, 27,  20, 21, 22, 23, 28, 29, 30, 31
 				// Convert int16 to int8
-				i0 = _mm256_packs_epi16( i0, i2 );	// 0, 1, 2, 3,  8, 9, 10, 11,  16, 17, 18, 19,  24, 25, 26, 27,  4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31
+				i0 = simde_mm256_packs_epi16( i0, i2 );	// 0, 1, 2, 3,  8, 9, 10, 11,  16, 17, 18, 19,  24, 25, 26, 27,  4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31
 
 				// We got our precious signed bytes, but the order is now wrong
 				// These AVX2 pack instructions process 16-byte pieces independently
 				// The following instruction is fixing the order
-				const __m256i perm = _mm256_setr_epi32( 0, 4, 1, 5, 2, 6, 3, 7 );
-				i0 = _mm256_permutevar8x32_epi32( i0, perm );
+				const simde__m256i perm = simde_mm256_setr_epi32( 0, 4, 1, 5, 2, 6, 3, 7 );
+				i0 = simde_mm256_permutevar8x32_epi32( i0, perm );
 
-				_mm256_storeu_si256(reinterpret_cast<__m256i *>(to_ptr[i].quants), i0);
+				simde_mm256_storeu_si256(to_ptr[i].quants, i0);
 			}
 		}
 	};
@@ -288,64 +288,64 @@ namespace spy::cpu {
 
 			for (size_t i = 0; i < num_from_block; ++i) {
 				// Load elements into 4 AVX vectors
-				__m256 v0 = _mm256_loadu_ps( from_ptr );
-				__m256 v1 = _mm256_loadu_ps( from_ptr + 8 );
-				__m256 v2 = _mm256_loadu_ps( from_ptr + 16 );
-				__m256 v3 = _mm256_loadu_ps( from_ptr + 24 );
+				simde__m256 v0 = simde_mm256_loadu_ps( from_ptr );
+				simde__m256 v1 = simde_mm256_loadu_ps( from_ptr + 8 );
+				simde__m256 v2 = simde_mm256_loadu_ps( from_ptr + 16 );
+				simde__m256 v3 = simde_mm256_loadu_ps( from_ptr + 24 );
 				from_ptr += 32;
 
 				// Compute max(abs(e)) for the block
-				const __m256 signBit = _mm256_set1_ps( -0.0F );
-				__m256 maxAbs = _mm256_andnot_ps( signBit, v0 );
-				maxAbs = _mm256_max_ps( maxAbs, _mm256_andnot_ps( signBit, v1 ) );
-				maxAbs = _mm256_max_ps( maxAbs, _mm256_andnot_ps( signBit, v2 ) );
-				maxAbs = _mm256_max_ps( maxAbs, _mm256_andnot_ps( signBit, v3 ) );
+				const simde__m256 signBit = simde_mm256_set1_ps( -0.0F );
+				simde__m256 maxAbs = simde_mm256_andnot_ps( signBit, v0 );
+				maxAbs = simde_mm256_max_ps( maxAbs, simde_mm256_andnot_ps( signBit, v1 ) );
+				maxAbs = simde_mm256_max_ps( maxAbs, simde_mm256_andnot_ps( signBit, v2 ) );
+				maxAbs = simde_mm256_max_ps( maxAbs, simde_mm256_andnot_ps( signBit, v3 ) );
 
-				__m128 max4 = _mm_max_ps( _mm256_extractf128_ps( maxAbs, 1 ), _mm256_castps256_ps128( maxAbs ) );
-					   max4 = _mm_max_ps( max4, _mm_movehl_ps( max4, max4 ) );
-					   max4 = _mm_max_ss( max4, _mm_movehdup_ps( max4 ) );
-				const float maxScalar = _mm_cvtss_f32( max4 );
+				simde__m128 max4 = simde_mm_max_ps( simde_mm256_extractf128_ps( maxAbs, 1 ), _mm256_castps256_ps128( maxAbs ) );
+					        max4 = simde_mm_max_ps( max4, simde_mm_movehl_ps( max4, max4 ) );
+					        max4 = simde_mm_max_ss( max4, simde_mm_movehdup_ps( max4 ) );
+				const float maxScalar = simde_mm_cvtss_f32( max4 );
 
 				// Quantize these floats
-				const  float delta = maxScalar / 127.0F;
-				to_ptr[i].delta    = spy_fp32_to_fp16(delta);
-				const  float id    = ( maxScalar != 0.0F ) ? 127.F / maxScalar : 0.0F;
-				const  __m256 mul  = _mm256_set1_ps( id );
+				const  float delta		= maxScalar / 127.0F;
+				to_ptr[i].delta			= spy_fp32_to_fp16(delta);
+				const  float id			= ( maxScalar != 0.0F ) ? 127.F / maxScalar : 0.0F;
+				const  simde__m256 mul  = simde_mm256_set1_ps( id );
 
 				// Apply the multiplier
-				v0 = _mm256_mul_ps( v0, mul );
-				v1 = _mm256_mul_ps( v1, mul );
-				v2 = _mm256_mul_ps( v2, mul );
-				v3 = _mm256_mul_ps( v3, mul );
+				v0 = simde_mm256_mul_ps( v0, mul );
+				v1 = simde_mm256_mul_ps( v1, mul );
+				v2 = simde_mm256_mul_ps( v2, mul );
+				v3 = simde_mm256_mul_ps( v3, mul );
 
 				// Round to nearest integer
-				v0 = _mm256_round_ps( v0, _MM_ROUND_NEAREST );
-				v1 = _mm256_round_ps( v1, _MM_ROUND_NEAREST );
-				v2 = _mm256_round_ps( v2, _MM_ROUND_NEAREST );
-				v3 = _mm256_round_ps( v3, _MM_ROUND_NEAREST );
+				v0 = simde_mm256_round_ps( v0, SIMDE_MM_ROUND_NEAREST );
+				v1 = simde_mm256_round_ps( v1, SIMDE_MM_ROUND_NEAREST );
+				v2 = simde_mm256_round_ps( v2, SIMDE_MM_ROUND_NEAREST );
+				v3 = simde_mm256_round_ps( v3, SIMDE_MM_ROUND_NEAREST );
 
 				// Convert floats to integers
-				__m256i i0 = _mm256_cvtps_epi32( v0 );
-				__m256i i1 = _mm256_cvtps_epi32( v1 );
-				__m256i i2 = _mm256_cvtps_epi32( v2 );
-				__m256i i3 = _mm256_cvtps_epi32( v3 );
+				simde__m256i i0 = simde_mm256_cvtps_epi32( v0 );
+				simde__m256i i1 = simde_mm256_cvtps_epi32( v1 );
+				simde__m256i i2 = simde_mm256_cvtps_epi32( v2 );
+				simde__m256i i3 = simde_mm256_cvtps_epi32( v3 );
 
 				// Compute the sum of the quants and set y[i].s
-				to_ptr[i].sum = spy_fp32_to_fp16(delta * hsum_i32_8(_mm256_add_epi32(_mm256_add_epi32(i0, i1), _mm256_add_epi32(i2, i3))));
+				to_ptr[i].sum = spy_fp32_to_fp16(delta * hsum_i32_8(simde_mm256_add_epi32(simde_mm256_add_epi32(i0, i1), simde_mm256_add_epi32(i2, i3))));
 
 				// Convert int32 to int16
-				i0 = _mm256_packs_epi32( i0, i1 );	// 0, 1, 2, 3,  8, 9, 10, 11,  4, 5, 6, 7, 12, 13, 14, 15
-				i2 = _mm256_packs_epi32( i2, i3 );	// 16, 17, 18, 19,  24, 25, 26, 27,  20, 21, 22, 23, 28, 29, 30, 31
+				i0 = simde_mm256_packs_epi32( i0, i1 );	// 0, 1, 2, 3,  8, 9, 10, 11,  4, 5, 6, 7, 12, 13, 14, 15
+				i2 = simde_mm256_packs_epi32( i2, i3 );	// 16, 17, 18, 19,  24, 25, 26, 27,  20, 21, 22, 23, 28, 29, 30, 31
 													// Convert int16 to int8
-				i0 = _mm256_packs_epi16( i0, i2 );	// 0, 1, 2, 3,  8, 9, 10, 11,  16, 17, 18, 19,  24, 25, 26, 27,  4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31
+				i0 = simde_mm256_packs_epi16( i0, i2 );	// 0, 1, 2, 3,  8, 9, 10, 11,  16, 17, 18, 19,  24, 25, 26, 27,  4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31
 
 				// We got our precious signed bytes, but the order is now wrong
 				// These AVX2 pack instructions process 16-byte pieces independently
 				// The following instruction is fixing the order
-				const __m256i perm = _mm256_setr_epi32( 0, 4, 1, 5, 2, 6, 3, 7 );
-				i0 = _mm256_permutevar8x32_epi32( i0, perm );
+				const simde__m256i perm = simde_mm256_setr_epi32( 0, 4, 1, 5, 2, 6, 3, 7 );
+				i0 = simde_mm256_permutevar8x32_epi32( i0, perm );
 
-				_mm256_storeu_si256(reinterpret_cast<__m256i *>(to_ptr[i].quants), i0);
+				simde_mm256_storeu_si256(to_ptr[i].quants, i0);
 			}
 		}
 	};
